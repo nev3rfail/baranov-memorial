93334e79c77396d1ced741dc9f04b4f52fe4f08c
Himjune <himjune@mail.ru>
2019-11-22 08:17:26 +0300

    icons for filter buttons

--- res/main.js
+++ res/main.js
@@ -109,7 +109,13 @@ function init(data) {
     /**
      * Templates
      */
-    const filter_item = `<li class="dropdown-item filter-link"><div class="px-1 py-1 d-inline-block">{text}</div><div class="d-inline-block float-right"><button class="btn btn-primary btn-sm ml-1 filter-btn" data-is_reverse="false" {data-tags}>+</button><button class="btn btn-primary btn-sm ml-1 filter-btn" data-is_reverse="true" {data-tags} >не</button></div></li>  `;
+    const filter_item = `<li class="dropdown-item filter-link"><div class="px-1 py-1 d-inline-block">{text}</div><div class="d-inline-block float-right">` +
+                        `<button class="btn btn-primary btn-sm ml-1 filter-btn" data-is_reverse="false" {data-tags}>` +
+                            `<span class="iconify" data-icon="ic-baseline-control-point" data-inline="false" style="font-size: 18px; font-wight: 400"></span>` +
+                        `</button><button class="btn btn-primary btn-sm ml-1 filter-btn" data-is_reverse="true" {data-tags} >` +
+                            `<span class="iconify" data-icon="ic:baseline-do-not-disturb-alt" data-inline="false" style="font-size: 18px; font-wight: 400"></span></span>` +
+                        `</button></div></li>`;
+
     const base_card = `<div class="col-xs-12 col-md-4 col-xl-3 pb-4 memorial-card-column">
             <div class="card memorial-card {nourl}" data-year="{year}" data-what="{where}">
                 {icon}
